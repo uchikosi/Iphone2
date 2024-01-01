e9d364b97ca84dc816d55ad10a1f3c81ba31d7dd
uchikosi yuuki <ucchikohiwithsugar@gmail.com>
2024-01-01 19:09:47 +0900

    コメント挿入

--- src/Mp3Player.java
+++ src/Mp3Player.java
@@ -1,8 +1,11 @@
 
 public interface Mp3Player {
+//	インターフェイス(interface) → ルールをプログラミング
 	public abstract void play();
 	public abstract void stop();
 	public abstract void next();
 	public abstract void back();
-
+//	abstract を使ってそれぞれの ルール(メソッド名のみ)を 作っています
+//	メソッド名のみで処理内容は書かないメソッドの事を「抽象メソッド」といいます。
+//	このため「abstract(抽象的な)」というキーワードをつけます。
 }
--- src/SmartPhone.java
+++ src/SmartPhone.java
@@ -1,3 +1,9 @@
 
 	public class SmartPhone extends Phone implements Mp3Player, NewFunction{
 	}
+//	インターフェイス(interface) → ルールをプログラミング
+//	extendsに継承、implementsに実装を行うクラスを書く
+//	クラスにインターフェイスを実装するには implements を指定しなく てはいけません。
+//	また Mp3Player で 作ったルール(メソッド)は必ず SmartPhone クラスで上書きし、処理 内容を記述する必要があります。
+//	Mp3Player がインタフェース（ルール）なので、必ず SmartPhone クラスには Mp3Playerで作ったルール(メソッド)と同じ
+//	メソッドを書き、さらに処理内容を書かなく てはいけません。
